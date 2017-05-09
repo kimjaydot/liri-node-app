f12debfc99396169d7c6a5aeb8d0eb57deb4c3f3
Kimberley Johnson <kimberleyjohnson@Kimberleys-MacBook-Pro.local>
2017-05-09 00:07:20 -0500

    add spotify and readFile

--- assets/js/liri.js
+++ assets/js/liri.js
@@ -1,13 +1,15 @@
 const keys = require('./keys.js');
+const fs = require('fs');
 const Twitter = require('twitter');
+const spotify = require('spotify');
 
 const command = process.argv[2];
 
 if (command === 'my-tweets') {
-    const c = new Twitter(keys);
-    const p = { screen_name: 'kimjaydot' }
+    const client = new Twitter(keys);
+    const parameters = { screen_name: 'kimjaydot' }
 
-    c.get('statuses/user_timeline', p, function(error, tweets, response) {
+    client.get('statuses/user_timeline', parameters, function(error, tweets, response) {
         if (!error) {
             if (!tweets.length) {
                 console.log('You have no tweets');
@@ -18,4 +20,19 @@ if (command === 'my-tweets') {
             console.log(error)
         }
     });
+} else if (command === 'spotify-this-song') {
+    const song = process.argv[3];
+
+    if (song) {
+        return
+    }
+
+    fs.readFile("../../random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.error(error);
+        }
+
+        // We will then print the contents of data
+        console.log(data);
+    })
 }
