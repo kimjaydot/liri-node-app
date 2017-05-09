54f41e97c62f342fbf965d7cdf011175ee827c63
Kimberley Johnson <kimberleyjohnson@Kimberleys-MacBook-Pro.local>
2017-05-08 23:02:08 -0500

    checking to see if my-tweets works but is getting a invalid or exired token

--- assets/js/keys.js
+++ assets/js/keys.js
@@ -1,6 +1,6 @@
 exports.twitterKeys = {
-  consumer_key: 'Xgui4XdFtqSCevQciHYO2lBtq',
-  consumer_secret: 'wCqx4n4yzgVHSgAuuMxHsz1IUiaujXOzDRDC5pvpUqxbAsfMC',
+  consumer_key: '8QtY6LGdmic8OvHkLkjRW9ksd',
+  consumer_secret: '9GCjS49zIltc5fHfWVhWFLZVUx6vPDmM6tn4HKHQ19s6e6FlTV',
   access_token_key: '28442829-7BUoVayQSKHxm4eFnI6iA163ZLy3hHFe23GvR4T2v',
   access_token_secret: 'yFLEpcoF32MU2HpA6NTk3mNZtyItVEar7QYHWbNf2PVkf',
 }
--- assets/js/liri.js
+++ assets/js/liri.js
@@ -1,6 +1,21 @@
 const keys = require('./keys.js');
+const Twitter = require('twitter');
+
 const command = process.argv[2];
 
-if (command === 'my-tweets'){
-    console.log(command);
+if (command === 'my-tweets') {
+    const c = new Twitter(keys);
+    const p = { screen_name: 'nodejs' }
+
+    c.get('statuses/user_timeline', p, function(error, tweets, response) {
+        if (!error) {
+            if (!tweets.length) {
+                console.log('You have no tweets');
+            }
+            console.log(tweets);
+        }
+        else {
+            console.log(error)
+        }
+    });
 }
