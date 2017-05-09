7abd4d98b6f7dc939b60307886634798c34a183f
Kimberley Johnson <kimberleyjohnson@Kimberleys-MacBook-Pro.local>
2017-05-08 21:32:07 -0500

    Added keys and tokens for Twitter

--- assets/js/keys.js
+++ assets/js/keys.js
@@ -1,9 +1,9 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'Xgui4XdFtqSCevQciHYO2lBtq',
+  consumer_secret: 'wCqx4n4yzgVHSgAuuMxHsz1IUiaujXOzDRDC5pvpUqxbAsfMC',
+  access_token_key: '28442829-7BUoVayQSKHxm4eFnI6iA163ZLy3hHFe23GvR4T2v',
+  access_token_secret: 'yFLEpcoF32MU2HpA6NTk3mNZtyItVEar7QYHWbNf2PVkf',
 }
 
